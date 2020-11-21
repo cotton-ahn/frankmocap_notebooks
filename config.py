a067352dcacc07a56148d248a063864494bd728b
cotton-ahn <cotton.ahn@gmail.com>
2020-11-21 19:34:24 +0100

    Update data and code

---
+++ config.py
@@ -0,0 +1,72 @@
+import os
+
+# change this value as you want. Set this as -1 to process with original fps.
+fps = -1
+vid_height = 640
+
+front_rear_cut = 500
+
+read_type = 'images'# 'videos' or 'images'
+
+# Some Path Information
+url_filePath = 'video_url.txt'
+video_dir = 'videos'
+image_dir = 'images'
+mocap_dir = 'mocaps'
+skeleton_pkl_dir = 'skeletons_pkl'
+skeleton_npz_dir = 'skeletons_npz'
+
+repo_abs_path = os.path.abspath('.')
+
+
+# Joint Names (./eft/eft/cores/jointorders.py)
+SPIN49_JOINT_NAMES = [
+                    'OP_Nose', 'OP_Neck', 'OP_RShoulder',           #0,1,2
+                    'OP_RElbow', 'OP_RWrist', 'OP_LShoulder',       #3,4,5
+                    'OP_LElbow', 'OP_LWrist', 'OP_MidHip',          #6, 7,8
+                    'OP_RHip', 'OP_RKnee', 'OP_RAnkle',             #9,10,11
+                    'OP_LHip', 'OP_LKnee', 'OP_LAnkle',             #12,13,14
+                    'OP_REye', 'OP_LEye', 'OP_REar',                #15,16,17
+                    'OP_LEar', 'OP_LBigToe', 'OP_LSmallToe',        #18,19,20
+                    'OP_LHeel', 'OP_RBigToe', 'OP_RSmallToe', 'OP_RHeel',  #21, 22, 23, 24  ##Total 25 joints  for openpose
+                    'Right Ankle', 'Right Knee', 'Right Hip',               #0,1,2
+                    'Left Hip', 'Left Knee', 'Left Ankle',                  #3, 4, 5
+                    'Right Wrist', 'Right Elbow', 'Right Shoulder',     #6
+                    'Left Shoulder', 'Left Elbow', 'Left Wrist',            #9
+                    'Neck (LSP)', 'Top of Head (LSP)',                      #12, 13
+                    'Pelvis (MPII)', 'Thorax (MPII)',                       #14, 15
+                    'Spine (H36M)', 'Jaw (H36M)',                           #16, 17
+                    'Head (H36M)', 'Nose', 'Left Eye',                      #18, 19, 20
+                    'Right Eye', 'Left Ear', 'Right Ear'                    #21,22,23 (Total 24 joints)
+                    ]
+
+
+open_pose_dict = dict()
+open_pose_dict['parent'] = [1, 0, 15, 0, 16,  1, 2, 3, 1, 5, 6, 1, 8, 9, 10, 11, 24, 24, 8, 12, 13, 14, 21, 21]
+open_pose_dict['child'] =  [0, 15,17, 16,18, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 24, 22, 23, 12,13, 14, 21, 19, 20]
+open_pose_dict['lr'] =     [0, 1, 1, 0, 0,   1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+open_pose_dict['joint_name'] = SPIN49_JOINT_NAMES[:25]
+
+simple_pose_dict = dict()
+simple_pose_dict['parent'] = [1, 1, 2, 3, 1, 5, 6, 1, 8, 9, 10, 8, 12, 13]
+simple_pose_dict['child'] =  [0, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12,13, 14]
+simple_pose_dict['lr'] =     [0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1,  0, 0, 0]
+simple_pose_dict['joint_name'] = SPIN49_JOINT_NAMES[:15]
+
+
+aux_pose_dict = dict()
+aux_pose_dict['parent'] = [12, 18, 19, 19, 21, 19, 20, 12, 8, 7, 12, 9, 10, 12, 15, 16, 14, 2, 1, 14, 3, 4]
+aux_pose_dict['child'] =  [18, 13, 17, 21, 23, 20, 22, 8,  7, 6, 9, 10, 11, 15, 16, 14, 2, 1, 0,  3,  4, 5]
+aux_pose_dict['lr'] =     [0,  0,  0,  1,  1,  0,  0,  1,  1, 1, 0,  0, 0, 0,   0,  0,  1, 1, 1, 0, 0, 0]
+aux_pose_dict['joint_name'] = SPIN49_JOINT_NAMES[25:]
+
+hand_dict = dict()
+hand_dict['parent'] = [0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15,0, 17, 18, 19]
+hand_dict['child'] =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11, 12, 13,14,15, 16, 17,18, 19, 20]
+hand_dict['joint_name'] = ['wrist', '5th_1', '5th_2', '5th_3', '5th_4',
+                           '4th_1', '4th_2', '4th_3', '4th_4',
+                           '3th_1', '3th_2', '3th_3', '3th_4',
+                           '2th_1', '2th_2', '2th_3', '2th_4',
+                           '1th_1', '1th_2', '1th_3', '1th_4']
+# param and function for plot
+lr_color = ['m', 'c']
---
+++ utils.py
@@ -0,0 +1,134 @@
+import config
+import numpy as np
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.patches as patches
+
+def plot_3D(body, left_hand, right_hand, body_dict, hand_dict, ax, draw_hand=True):
+    body = body.reshape(-1, 3)
+    for ii, p_idx in enumerate(body_dict['parent']):
+        ax.plot([body[p_idx, 0], body[body_dict['child'][ii], 0]],
+                [body[p_idx, 1], body[body_dict['child'][ii], 1]],
+                [body[p_idx, 2], body[body_dict['child'][ii], 2]],
+                color=config.lr_color[body_dict['lr'][ii]])
+        
+    for j_i, joint in enumerate(body):
+        ax.plot(joint[0], joint[1], joint[2], 'k.')
+        ax.text(joint[0]-0.1, joint[1]-0.05, joint[2]-0.1, str(j_i), color='b', fontsize=10)
+
+    if draw_hand:
+        for ii, p_idx in enumerate(hand_dict['parent']):
+            ax.plot([left_hand[p_idx, 0], left_hand[hand_dict['child'][ii], 0]],
+                    [left_hand[p_idx, 1], left_hand[hand_dict['child'][ii], 1]],
+                    [left_hand[p_idx, 2], left_hand[hand_dict['child'][ii], 2]],
+                    color=config.lr_color[0], linewidth=2)
+
+            ax.plot([right_hand[p_idx, 0], right_hand[hand_dict['child'][ii], 0]],
+                    [right_hand[p_idx, 1], right_hand[hand_dict['child'][ii], 1]],
+                    [right_hand[p_idx, 2], right_hand[hand_dict['child'][ii], 2]],
+                    color=config.lr_color[1], linewidth=2)
+        
+    ax.view_init(-90, -90)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    
+
+def plot_2D(body, body_dict, 
+            left_hand, right_hand, hand_dict,
+            bbox, ax, draw_hand=True):  
+    body = body.reshape(-1, 3)
+    for ii, p_idx in enumerate(body_dict['parent']):
+        ax.plot([body[p_idx, 0], body[body_dict['child'][ii], 0]],
+                [body[p_idx, 1], body[body_dict['child'][ii], 1]],
+                color=config.lr_color[body_dict['lr'][ii]], linewidth=2)
+        
+    if draw_hand:
+        for ii, p_idx in enumerate(hand_dict['parent']):
+            ax.plot([left_hand[p_idx, 0], left_hand[hand_dict['child'][ii], 0]],
+                    [left_hand[p_idx, 1], left_hand[hand_dict['child'][ii], 1]],
+                    color=config.lr_color[0], linewidth=2)
+
+            ax.plot([right_hand[p_idx, 0], right_hand[hand_dict['child'][ii], 0]],
+                    [right_hand[p_idx, 1], right_hand[hand_dict['child'][ii], 1]],
+                    color=config.lr_color[1], linewidth=2)
+            
+    # Create a Rectangle patch
+    rect = patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3],
+                             linewidth=2, edgecolor='r', facecolor='none')
+
+    # Add the patch to the Axes
+    ax.add_patch(rect)
+    
+    
+def plot_vector(pos, vec, ax, length=50):
+    ax.plot([pos[0], pos[0]+length*vec[0]], [pos[1], pos[1]+length*vec[1]], linewidth=5)
+    
+
+def get_head_info(open_pose):
+    head_info = dict()
+    head_info['pos'] = open_pose[0]
+    head_info['vec'] = open_pose[0] - (open_pose[17] + open_pose[18])/2
+    head_info['vec'] /= np.linalg.norm(head_info['vec'])
+    
+    info_array = np.concatenate([head_info['pos'], head_info['vec']])
+    return head_info, info_array
+
+def plot_head_info(head_info, ax):
+    plot_vector(head_info['pos'], head_info['vec'], ax)
+
+
+def get_hand_info(hand):
+    hand_info = dict()
+    
+    index_vector = hand[9] - hand[0]
+    index_vector /= np.linalg.norm(index_vector)
+
+    thumb_vector = hand[17] - hand[0]
+    thumb_vector /= np.linalg.norm(thumb_vector)
+
+    palm_vector = np.cross(index_vector, thumb_vector)
+    palm_vector /= np.linalg.norm(palm_vector)
+
+    new_thumb_vector = np.cross(index_vector, palm_vector)
+    new_thumb_vector /= np.linalg.norm(new_thumb_vector)
+    
+    hand_info['thumb'] = new_thumb_vector
+    hand_info['index'] = index_vector
+    hand_info['palm'] = palm_vector
+    hand_info['pos'] = hand[0]
+    
+    info_array = [new_thumb_vector, index_vector, palm_vector, hand[0]]
+    info_array = np.concatenate(info_array)
+    
+    return hand_info, info_array
+
+def plot_hand_info(hand_info, ax):
+    plot_vector(hand_info['pos'], hand_info['thumb'], ax)
+    plot_vector(hand_info['pos'], hand_info['index'], ax)
+    plot_vector(hand_info['pos'], hand_info['palm'], ax)
+
+    
+def get_heel_info(open_pose):
+    left_heel = open_pose[21]
+    right_heel = open_pose[24]
+    
+    left_foot_vector = (open_pose[19] + open_pose[20])/2 - open_pose[21]
+    left_foot_vector /= np.linalg.norm(left_foot_vector)
+
+    right_foot_vector = (open_pose[22] + open_pose[23])/2 - open_pose[24]
+    right_foot_vector /= np.linalg.norm(right_foot_vector)
+
+    left = {'pos':left_heel, 'vec':left_foot_vector}
+    right = {'pos':right_heel, 'vec':right_foot_vector}
+    
+    info_array = [left_heel, left_foot_vector, right_heel, right_foot_vector]
+    info_array = np.concatenate(info_array)
+    
+    return left, right, info_array
+    
+def plot_heel_info(left, right, ax): 
+    plot_vector(left['pos'], left['vec'], ax)
+    plot_vector(right['pos'], right['vec'], ax)
+    
